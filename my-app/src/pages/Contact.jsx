ea82ec1e84f5af839666dcdd511a6366d025a533
dilmi214 <dilmifernando214@gmail.com>
2025-03-14 13:34:05 +0530

    added loading spinners to home and contact

--- my-app/src/pages/Contact.jsx
+++ my-app/src/pages/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import '../styles/contact.css'
 import InstaIcon from '../assets/instagram.png'
 import LinkedInIcon from '../assets/linkedin.png'
@@ -13,51 +13,71 @@ const Contact = () => {
   const instaLink = location.state?.insta || "Default Link";
   const mailLink = location.state?.mail || "Default Link";
 
-  return (
-    <div className="container">
-      <div className="contact-intro">
-        <div className="bg-image">
-          
-        </div>
-        <div className="contact-intro-text">
-          <h1>Contact Me</h1>
-          <p>I’d love to hear from you! Whether you have questions, feedback, 
-            or just want to connect, feel free to reach out. 
-            Your thoughts are always welcome.</p>
-        </div>
-      </div>
-      <div className="contact-card">
-        <div className="left-section">
-          <div className='get-in-touch-container'>
-            Get in <span className="span-contact">touch</span>
-          </div>
-          <div className="socials-container">
-                <a href= { instaLink } target='_blank'> <img src={ InstaIcon } alt="instagram icon" /></a>
-                <a href = { linkedinLink } target='_blank'> <img  src={ LinkedInIcon } alt="linkedin icon" /></a>
-                <a href= { githubLink } target='_blank'> <img src={ GithubIcon } alt="github icon" /></a>
-                <a href= { mailLink } target='_blank'> <img src={ MailIcon } alt="mail icon" /></a>
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        setLoading(false); // Hide the spinner after 0.5s 
+      }, 500); 
+  
+      return () => clearTimeout(timer); // Cleanup timeout
+    }, []);
 
+  return (
+    <div>
+      {/* Show spinner while loading */}
+      {loading && (
+        <div className="loading-spinner">
+          <div className="spinner"></div>
+        </div>
+      )}
+      {!loading && (
+        <div className="container">
+          <div className="contact-intro">
+            <div className="bg-image">
+              
+            </div>
+            <div className="contact-intro-text">
+              <h1>Contact Me</h1>
+              <p>I’d love to hear from you! Whether you have questions, feedback, 
+                or just want to connect, feel free to reach out. 
+                Your thoughts are always welcome.</p>
+            </div>
+          </div>
+          <div className="contact-card">
+            <div className="left-section">
+              <div className='get-in-touch-container'>
+                Get in <span className="span-contact">touch</span>
+              </div>
+              <div className="socials-container">
+                    <a href= { instaLink } target='_blank'> <img src={ InstaIcon } alt="instagram icon" /></a>
+                    <a href = { linkedinLink } target='_blank'> <img  src={ LinkedInIcon } alt="linkedin icon" /></a>
+                    <a href= { githubLink } target='_blank'> <img src={ GithubIcon } alt="github icon" /></a>
+                    <a href= { mailLink } target='_blank'> <img src={ MailIcon } alt="mail icon" /></a>
+
+              </div>
+            </div>
+            <div className="right-section">
+              <div className="form-group">
+                <div className="input-container">
+                  <label className="form-tag">Name</label>
+                  <input type="text" name="name" className="form-input" placeholder="Enter your name" />
+                </div>
+                <div className="input-container">
+                  <label className="form-tag">Mail</label>
+                  <input type="text" name="email" className="form-input" placeholder="Enter your email" />
+                </div>
+                <div className="input-container">
+                  <label className="form-tag">Inquiry</label>
+                  <textarea rows="10" name="inquiry" className="form-input" placeholder="Enter your inquiry"></textarea>
+                </div>
+                <button className='submit-button'>Submit</button>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="right-section">
-          <div className="form-group">
-            <div className="input-container">
-              <label className="form-tag">Name</label>
-              <input type="text" name="name" className="form-input" placeholder="Enter your name" />
-            </div>
-            <div className="input-container">
-              <label className="form-tag">Mail</label>
-              <input type="text" name="email" className="form-input" placeholder="Enter your email" />
-            </div>
-            <div className="input-container">
-              <label className="form-tag">Inquiry</label>
-              <textarea rows="10" name="inquiry" className="form-input" placeholder="Enter your inquiry"></textarea>
-            </div>
-            <button className='submit-button'>Submit</button>
-          </div>
-        </div>
+      )}
     </div>
-  </div>
   )
 }
 
--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,31 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import HomeGirl from '../assets/home-girl.png'
 import '../styles/Home.css'
 import { Link } from 'react-router-dom'
 
 
+
 const Home = () => {
   const CV = 'https://drive.google.com/uc?export=download&id=1gLjRF0ROA5W8Q0L_D4KqC1Qqz3t55hni';
-  return (
-    <div className='container'>
-      <div className="section1">
-        <div className="girl-img-container">
-        <img className='girlIMG' src={ HomeGirl } alt='Image of a girl'/>
-        </div>
-        <div className="intro-text">
-          <h1>Hello, I'm <span className='fancy-text'>Dil</span>.</h1>
-          <p>Passionate about problem-solving, innovation, and continuous learning. 
-            I contribute to open-source communities and enjoy taking on new challenges in software development.</p>
+  const [loading, setLoading] = useState(true);
 
-            <a href= { CV } download><button className="cv-button">Download CV</button></a>
-            <Link to='/contact'><button className="contact-button">Contact Me</button></Link>
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); // Hide the spinner after 0.3s 
+    }, 300); 
+
+    return () => clearTimeout(timer); // Cleanup timeout
+  }, []);
+
+  return (
+    <div>
+      {/* Show spinner while loading */}
+      {loading && (
+        <div className="loading-spinner">
+          <div className="spinner"></div>
         </div>
-      </div>
-      {/* <div className="section2">
-        <div className="skills-container">
-          <h1>My Skills</h1>
+      )}
+      {!loading && (
+        <div className='container'>
+          <div className="section1">
+            <div className="girl-img-container">
+            <img className='girlIMG' src={ HomeGirl } alt='Image of a girl'/>
+            </div>
+            <div className="intro-text">
+              <h1>Hello, I'm <span className='fancy-text'>Dil</span>.</h1>
+              <p>Passionate about problem-solving, innovation, and continuous learning. 
+                I contribute to open-source communities and enjoy taking on new challenges in software development.</p>
+
+                <a href= { CV } download><button className="cv-button">Download CV</button></a>
+                <Link to='/contact'><button className="contact-button">Contact Me</button></Link>
+            </div>
+          </div>
+          {/* <div className="section2">
+            <div className="skills-container">
+              <h1>My Skills</h1>
+            </div>
+          </div> */}
         </div>
-      </div> */}
+      )}
     </div>
   )
 }
