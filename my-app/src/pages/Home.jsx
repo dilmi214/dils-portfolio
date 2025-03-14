e0386e40f276602e6b5496debd10d6bcb2b54027
dilmi214 <dilmifernando214@gmail.com>
2025-03-14 09:35:24 +0530

    added download cv and contact buttons to home with functionality

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import HomeGirl from '../assets/home-girl.png'
 import '../styles/Home.css'
+import CV from '../assets/Dilmi_SE_INTERN_CV.pdf'
+import { Link } from 'react-router-dom'
+
 const Home = () => {
   return (
     <div className='container'>
@@ -12,13 +15,16 @@ const Home = () => {
           <h1>Hello, I'm <span className='fancy-text'>Dil</span>.</h1>
           <p>Passionate about problem-solving, innovation, and continuous learning. 
             I contribute to open-source communities and enjoy taking on new challenges in software development.</p>
+
+            <a href= { CV } download><button className="cv-button">Download CV</button></a>
+            <Link to='/contact'><button className="contact-button">Contact Me</button></Link>
         </div>
       </div>
-      <div className="section2">
+      {/* <div className="section2">
         <div className="skills-container">
           <h1>My Skills</h1>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
