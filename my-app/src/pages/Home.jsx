05914a2cba32d5bf828e5f9d1f9e67ce54ece3e7
dilmi214 <dilmifernando214@gmail.com>
2025-03-14 08:44:03 +0530

    section 1 in home page okay

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import WelcomeGIF from '../assets/Welcome.gif'
+import HomeGirl from '../assets/home-girl.png'
+import '../styles/Home.css'
 const Home = () => {
   return (
-    <div className='container'>Home</div>
+    <div className='container'>
+      <div className="section1">
+        <div className="girl-img-container">
+        <img className='girlIMG' src={ HomeGirl } alt='Image of a girl'/>
+        </div>
+        <div className="intro-text">
+          <h1>Hello, I'm <span className='fancy-text'>Dil</span>.</h1>
+          <p>Passionate about problem-solving, innovation, and continuous learning. 
+            I contribute to open-source communities and enjoy taking on new challenges in software development.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
