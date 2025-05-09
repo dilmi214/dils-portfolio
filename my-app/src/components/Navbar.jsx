959b40dc279aa7022e00196ee879ab066c2b81a7
dilmi214 <dilmifernando214@gmail.com>
2025-05-09 14:35:22 +0530

    made nav bar mobile responsive

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -2,19 +2,29 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../components/Navbar.css';
 import { socialLinks } from '../assets/links';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className='navbar'>
-        <p className='fancy-text'>Dil's Space</p>
-        <ul>
-            <Link to='/'><li>Home</li></Link>
-            <Link to='/projects'><li>Projects</li></Link>
-            {/* <Link to = '/beyond-code'><li>Beyond Code</li></Link> */}
-            <Link to={`/contact?github=${encodeURIComponent(socialLinks.github)}&linkedin=${encodeURIComponent(socialLinks.linkedin)}&insta=${encodeURIComponent(socialLinks.insta)}&mail=${encodeURIComponent(socialLinks.mail)}`}>Contact</Link>
-        </ul>
-    </div>
-  )
-}
+        <div className="navbar-header">
+          <p className="fancy-text">Dil's Space</p>
+          <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            ☰
+          </button>
+        </div>
 
-export default Navbar
+        <ul className={`nav-links ${menuOpen ? 'show' : ''}`}>
+
+        <Link to='/'><li>Home</li></Link>
+        <Link to='/projects'><li>Projects</li></Link>
+        {/* <Link to='/beyond-code'><li>Beyond Code</li></Link> */}
+        <Link to={`/contact?github=${encodeURIComponent(socialLinks.github)}&linkedin=${encodeURIComponent(socialLinks.linkedin)}&insta=${encodeURIComponent(socialLinks.insta)}&mail=${encodeURIComponent(socialLinks.mail)}`}>Contact</Link>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
