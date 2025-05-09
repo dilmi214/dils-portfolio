0148b6d7a50a5f0e1d4bbee94a4c5bc11a323c3a
dilmi214 <dilmifernando214@gmail.com>
2025-05-09 10:21:43 +0530

    removed beyond code temporarily

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <ul>
             <Link to='/'><li>Home</li></Link>
             <Link to='/projects'><li>Projects</li></Link>
-            <Link to = '/beyond-code'><li>Beyond Code</li></Link>
+            {/* <Link to = '/beyond-code'><li>Beyond Code</li></Link> */}
             <Link to={`/contact?github=${encodeURIComponent(socialLinks.github)}&linkedin=${encodeURIComponent(socialLinks.linkedin)}&insta=${encodeURIComponent(socialLinks.insta)}&mail=${encodeURIComponent(socialLinks.mail)}`}>Contact</Link>
         </ul>
     </div>
